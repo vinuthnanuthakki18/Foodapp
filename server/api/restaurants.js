797bf4d2a54b9724633deecc364eeea95238a27e
Vinuthna <vinuthnanuthakki18@gmail.com>
2025-03-16 14:59:54 +0530

    changed layout

--- server/api/restaurants.js
+++ server/api/restaurants.js
@@ -29,7 +29,7 @@ router.post('/search', async (req, res) => {
       name : {$regex : query, $options : "i"}
     });
 // console.log(returnRestaurants.length, page, items, startIndex );
-    // console.log(Restaurantss);
+    console.log(Restaurantss);
   res.json({ data : Restaurantss, total : Restaurantss.length});
 }catch (error) {
   res.status(500).json({ message: "Error fetching restaurants", error });
