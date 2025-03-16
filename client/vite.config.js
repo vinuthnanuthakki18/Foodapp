9980276d7eac363e472be1de64c768da3b2cedc8
Vinuthna <vinuthnanuthakki18@gmail.com>
2025-03-16 15:34:16 +0530

    changed layout

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["lodash"], // Ensure lodash is treated as an external dependency
+    },
+  },
 })
